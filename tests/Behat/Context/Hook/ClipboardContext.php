45840a54c764329d946f5613b5f06be0b06a3b5a
Kevin Kaniaburka <kevin.kaniaburka@gmail.com>
2023-03-18 02:30:16 +0100

    Introduce ClipboardContext hook to share data between many Behat contexts

---
+++ tests/Behat/Context/Hook/ClipboardContext.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Behat\Context\Hook;
+
+use Behat\Behat\Context\Context;
+
+final class ClipboardContext implements Context
+{
+    private array $clipboard = [];
+
+    public function copy(string $key, mixed $value): void
+    {
+        $this->clipboard[$key] = $value;
+    }
+
+    public function paste(string $key): mixed
+    {
+        return $this->clipboard[$key];
+    }
+}
