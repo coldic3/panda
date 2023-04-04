1cb6116491aad8f2f4c4cf7f9323574f95bdec12
Kevin Kaniaburka <kevin.kaniaburka@gmail.com>
2023-04-05 00:48:10 +0200

    Add TransactionFactory

---
+++ src/Transaction/Domain/Factory/TransactionFactoryInterface.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Panda\Transaction\Domain\Factory;
+
+use Panda\Contract\AggregateRoot\Owner\OwnerInterface;
+use Panda\Transaction\Domain\Model\TransactionInterface;
+use Panda\Transaction\Domain\ValueObject\OperationInterface;
+
+interface TransactionFactoryInterface
+{
+    /**
+     * @param OperationInterface[] $adjustments
+     */
+    public function createAsk(
+        OperationInterface $from,
+        OperationInterface $to,
+        array $adjustments,
+        \DateTimeInterface $concludedAt,
+        ?OwnerInterface $owner = null,
+    ): TransactionInterface;
+
+    /**
+     * @param OperationInterface[] $adjustments
+     */
+    public function createBid(
+        OperationInterface $from,
+        OperationInterface $to,
+        array $adjustments,
+        \DateTimeInterface $concludedAt,
+        ?OwnerInterface $owner = null,
+    ): TransactionInterface;
+
+    /**
+     * @param OperationInterface[] $adjustments
+     */
+    public function createDeposit(
+        OperationInterface $to,
+        array $adjustments,
+        \DateTimeInterface $concludedAt,
+        ?OwnerInterface $owner = null,
+    ): TransactionInterface;
+
+    /**
+     * @param OperationInterface[] $adjustments
+     */
+    public function createWithdraw(
+        OperationInterface $from,
+        array $adjustments,
+        \DateTimeInterface $concludedAt,
+        ?OwnerInterface $owner = null,
+    ): TransactionInterface;
+
+    /**
+     * @param OperationInterface[] $adjustments
+     */
+    public function createTransfer(
+        OperationInterface $to,
+        array $adjustments,
+        \DateTimeInterface $concludedAt,
+        ?OwnerInterface $owner = null,
+    ): TransactionInterface;
+
+    /**
+     * @param OperationInterface[] $adjustments
+     */
+    public function createFee(
+        array $adjustments,
+        \DateTimeInterface $concludedAt,
+        ?OwnerInterface $owner = null,
+    ): TransactionInterface;
+}
