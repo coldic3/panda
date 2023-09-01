097a7034dfb0b55c456b855160b1d941771d05ca
Kevin Kaniaburka <kevin.kaniaburka@gmail.com>
2023-09-01 03:27:10 +0200

    Implement ReportRepository

--- src/Portfolio/Infrastructure/Configuration/Symfony/Service/repository.php
+++ src/Portfolio/Infrastructure/Configuration/Symfony/Service/repository.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 use Doctrine\ORM\EntityManagerInterface;
 use Panda\Portfolio\Domain\Repository\PortfolioItemRepositoryInterface;
 use Panda\Portfolio\Domain\Repository\PortfolioRepositoryInterface;
+use Panda\Portfolio\Domain\Repository\ReportRepositoryInterface;
 use Panda\Portfolio\Infrastructure\Doctrine\Orm\PortfolioItemRepository;
 use Panda\Portfolio\Infrastructure\Doctrine\Orm\PortfolioRepository;
+use Panda\Portfolio\Infrastructure\Doctrine\Orm\ReportRepository;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 
@@ -19,5 +21,9 @@ return static function (ContainerConfigurator $containerConfigurator): void {
 
     $services->set(PortfolioItemRepositoryInterface::class)
         ->class(PortfolioItemRepository::class)
+        ->args([service(EntityManagerInterface::class)]);
+
+    $services->set(ReportRepositoryInterface::class)
+        ->class(ReportRepository::class)
         ->args([service(EntityManagerInterface::class)]);
 };
